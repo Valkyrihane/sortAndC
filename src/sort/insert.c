fdeef43487339b8dfcc8b0c6a6f5a35b4f02c010
Valky <anatole.juge@epitech.eu>
2018-04-01 02:28:31 +0200

    added insertion sort

--- src/sort/insert.c
+++ src/sort/insert.c
@@ -4,6 +4,15 @@
 
 void insert_sort(ssize_t *tab, size_t size)
 {
-	(void)tab;
-	(void)size;
+	size_t extracted = 0;
+
+	for (size_t idx = 0; idx < size; idx++) {
+		extracted = idx + 1;
+		while (extracted &&
+		       extracted < size &&
+		       tab[extracted] < tab[extracted - 1]) {
+			SWAP(tab[extracted], tab[extracted - 1]);
+			--extracted;
+		}
+	}
 }
